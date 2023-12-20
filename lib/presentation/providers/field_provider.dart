fc475b837b373aa8773ad4e7fe6ae977eb6c4421
fritz <franciscosantin2002@gmail.com>
2023-12-19 21:13:22 -0300

    riverpod agregado

---
+++ lib/presentation/providers/field_provider.dart
@@ -0,0 +1,3 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final fieldProvider = StateProvider((ref) => []);
--- lib/presentation/screens/gameStart/game_start_screen.dart
+++ lib/presentation/screens/gameStart/game_start_screen.dart
@@ -1,25 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:tutti_frutti/presentation/providers/field_provider.dart';
 import 'package:tutti_frutti/presentation/screens.dart';
 
-class GameStartScreen extends StatefulWidget {
+class GameStartScreen extends ConsumerWidget {
   static const String name = 'game_start';
   const GameStartScreen({super.key});
 
   @override
-  State<GameStartScreen> createState() => _GameStartScreenState();
-}
-
-class _GameStartScreenState extends State<GameStartScreen> {
-  List<String> columnas = [];
-
-  void agregarColumna(String col) {
-    columnas.add(col);
-    setState(() {});
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final fields = ref.watch(fieldProvider);
     return Scaffold(
         appBar: AppBar(title: const Text('Comenzar Partida')),
         body: SingleChildScrollView(
@@ -27,7 +18,7 @@ class _GameStartScreenState extends State<GameStartScreen> {
             children: [
               TextField(
                 onSubmitted: (value) {
-                  agregarColumna(value);
+                  ref.read(fieldProvider.notifier).state.add(value);
                 },
               ),
               const SizedBox(height: 20),
@@ -36,9 +27,15 @@ class _GameStartScreenState extends State<GameStartScreen> {
                     context.pushNamed(GameScreen.name);
                   },
                   child: const Text('Listo')),
-              ...columnas.map((col) => ListTile(
+              //SingleChildScrollView(
+              //  child: ListView.builder(
+              //      itemBuilder: (context, index) => ListTile(
+              //          leading: const Icon(Icons.data_array_sharp),
+              //          title: Text(fields[index]))),
+              //)
+              ...fields.map((field) => ListTile(
                   leading: const Icon(Icons.data_array_sharp),
-                  title: Text(col)))
+                  title: Text(field)))
             ],
           ),
         ));
